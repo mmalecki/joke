dcfa4ea244a3dff271b1a621655d53d66985e841
Maciej Małecki <me@mmalecki.com>
2013-10-30 13:30:00 +0100

    [doc] Only `stringify` in JSON example

--- examples/json.js
+++ examples/json.js
@@ -2,12 +2,8 @@ var es = require('event-stream'),
     util = require('util'),
     joke = require('../')();
 
-function inspect(obj) {
-  return util.inspect(obj, { colors: true });
-}
-
 joke
-  .pipe(es.mapSync(inspect))
+  .pipe(es.mapSync(JSON.stringify))
   .pipe(es.join('\n'))
   .pipe(process.stdout);
 
